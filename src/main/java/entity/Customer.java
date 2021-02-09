db06d499764c04ab3c7c521d938a1914c8d85135
anhnbt-it <anhnbt.it@gmail.com>
2021-02-09 07:25:20 +0700

    Refactor field Customer

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -2,7 +2,13 @@ package entity;
 
 import java.time.LocalDate;
 
-public class Customer extends Person {
+public class Customer {
     private Integer id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private Address address;
+    private String username;
+    private String password;
     private LocalDate createAt;
 }
